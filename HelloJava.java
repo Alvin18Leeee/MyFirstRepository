5418073a3f7583fae1a0f5be67716bf92a2cc92a
Alvin18Leeee <Alvin_Leeee@hotmail.com>
2019-05-09 11:44:12 +0800

    my first Java program

---
+++ HelloJava.java
@@ -0,0 +1,13 @@
+package Hello;
+
+public class HelloJava {
+	private static String book = "《Java从入门到精通》";
+	private static String saying = "是我学习Java的第一本书"; 
+	
+	public static void main(String[] args) {
+		System.out.println(book);
+		System.out.println(saying);
+
+	}
+
+}
